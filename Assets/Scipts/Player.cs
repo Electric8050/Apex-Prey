6b049e959d0a2517c3c39c97601a518dd80b8f11
MUKILTEO\1728278 <electric8050@gmail.com>
2026-07-02 12:03:09 -0700

    Added Sprinting

--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -7,10 +7,14 @@ public class Playercube : MonoBehaviour
     public float speed;
     public Rigidbody rb;
     public bool isOnGround = true;
+    public float walkSpeed = 10f;
+    public float sprintSpeed = 15f;
+    private float currentSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        currentSpeed = walkSpeed;
     }
 
     // Update is called once per frame
@@ -18,25 +22,30 @@ public class Playercube : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // update currentSpeed before moving so sprint takes effect immediately
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+
+        // use currentSpeed for movement
+        transform.Translate(Vector3.right * Time.deltaTime * currentSpeed * horizontalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed * forwardInput);
 
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
-
         {
-
-
             rb.AddForce(Vector3.up * 700);
-
             isOnGround = false;
-
         }
     }
     private void OnCollisionEnter(Collision collision)
-
     {
         if (collision.gameObject.CompareTag("Ground"))
-
         {
             isOnGround = true;
         }
